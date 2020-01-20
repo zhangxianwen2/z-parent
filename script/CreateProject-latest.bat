40ffc44dd676a3f2973526c06a90f90795b7728e
zhangxianwen <zhangxianwen@51xf.cn>
2020-01-20 18:04:50 +0800

    [调整]调整文件名

--- script/CreateProject-latest.bat
+++
@@ -1,6 +0,0 @@
-mvn archetype:generate ^
-    -DarchetypeGroupId=com.seven ^
-    -DarchetypeArtifactId=z-boot-starter-archetype ^
-    -DarchetypeVersion=1.0-SNAPSHOT ^
-    -DgroupId=com.seven ^
-    -DartifactId=z-archetype-test
